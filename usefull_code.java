7cafb0106a89eec0a90127dc50afdf0105eb9894
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-05-17 10:28:15 +0100

    Update usefull_code.java
    Modificar o update

--- usefull_code.java
+++ usefull_code.java
@@ -1,1 +1,9 @@
 
+## Modificar o update
+@Modifying
+@Transactional
+@Query(
+  nativeQuery = true,
+  value ="UPDATE table_name SET column_name = :name where id = :id"
+)
+void update(@Param("name") String name, @Param("id") Integer id);
