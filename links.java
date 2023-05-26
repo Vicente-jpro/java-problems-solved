d81c9849fb59cb17e66b511abaeb652458355c8a
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-05-26 17:21:57 +0100

    How to change jdk version in Visual Studio Code

--- links.java
+++ links.java
@@ -1,2 +1,5 @@
 //Spring Boot Starter Parent versions
 https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-parent
+
+// How to change jdk version in Visual Studio Code
+https://code.visualstudio.com/docs/java/java-project#_configure-runtime-for-projects
