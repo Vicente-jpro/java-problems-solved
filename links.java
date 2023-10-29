51587629451d850aa8004a46854e903ce2d854b7
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-10-29 15:01:32 +0100

    Update links.java
    How to set JAVA_HOME enviroment

--- links.java
+++ links.java
@@ -3,3 +3,6 @@ https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-
 
 // How to change jdk version in Visual Studio Code
 https://code.visualstudio.com/docs/java/java-project#_configure-runtime-for-projects
+
+// How to set JAVA_HOME enviroment 
+https://stackoverflow.com/questions/9612941/how-to-set-java-environment-path-in-ubuntu
