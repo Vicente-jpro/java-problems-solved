e2092e13eff1edefcf3bead96a1153fa9142f477
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-01-24 18:40:19 +0100

    Update javaee.java

--- javaee.java
+++ javaee.java
@@ -1,4 +1,3 @@
 # java.lang.ClassNotFoundException: javax.faces.webapp.FacesServlet
 https://stackoverflow.com/questions/22658185/java-lang-classnotfoundexception-javax-faces-webapp-facesservlet
-  
 "Project Properties" --> "Deployment Assembly", adding "Java Build Path Entries" -> "Maven Dependencies" solves the problem!
