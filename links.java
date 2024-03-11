2559eda64b08d184454a21e960834e9e09bc5cbe
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-03-11 01:46:40 +0100

    Update links.java

--- links.java
+++ links.java
@@ -6,3 +6,6 @@ https://code.visualstudio.com/docs/java/java-project#_configure-runtime-for-proj
 
 // How to set JAVA_HOME enviroment 
 https://stackoverflow.com/questions/9612941/how-to-set-java-environment-path-in-ubuntu
+
+Interview questions
+https://www.youtube.com/watch?v=kJp8BiEq-P8&list=PLSa9-rMh6lhEE-iGkvLGXio1_h29OMVZp&index=8
