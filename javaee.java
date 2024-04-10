98438d2e7e42249a7e974a8f5c70495d7001115f
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-04-10 18:40:40 +0100

    Update javaee.java

--- javaee.java
+++ javaee.java
@@ -1,3 +1,6 @@
 # java.lang.ClassNotFoundException: javax.faces.webapp.FacesServlet
 https://stackoverflow.com/questions/22658185/java-lang-classnotfoundexception-javax-faces-webapp-facesservlet
 "Project Properties" --> "Deployment Assembly", adding "Java Build Path Entries" -> "Maven Dependencies" solves the problem!
+
+# JSF tag is no showing
+https://www.tutorialspoint.com/jsf/jsf_first_application.html
